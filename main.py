02138e3dea0eee8b2f8c34c2086cb3cbb34bafe1
Klondayk <77169470+Klondayk@users.noreply.github.com>
2023-01-05 03:35:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -70,5 +70,4 @@ async def refresh_msg(nickname, msg_box):
 
 if __name__ == "__main__":
     #start_server(main, debug=True, port=3000, cdn=False)
-    start_server(partial(main, raft_addr=raft_addr, cluster=cluster),
-                 debug=False, auto_open_webbrowser=True, port=int(os.environ.get("WEB_PORT", 0)))
+    start_server(main,debug=False, auto_open_webbrowser=True, port=int(os.environ.get("WEB_PORT", 0)))
